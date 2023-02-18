e7c10b60c1e11c53bb0bc6766f2c8567b086066d
iresville <iresgh1@gmail.com>
2023-02-18 00:05:14 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+main - Entry point
+Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;  
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-        return (0);
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
 ~                     
