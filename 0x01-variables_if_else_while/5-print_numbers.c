96a09751cd47f6a23115fe162c712077c65eed87
iresville <iresgh1@gmail.com>
2023-02-18 20:49:11 -0800

    printalphabets

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-main -Entry point
-Description: prints allsingle numbers of base 10 from 0
-Return: Always 0 (success)
+*main -Entry point
+*Description: prints all single numbers of base 10 from 0
+*Return: Always 0 (success)
 */
 int main(void)
 {
