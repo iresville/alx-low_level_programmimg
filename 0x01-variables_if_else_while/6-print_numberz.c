29aabf0149673867e0296b8da0a8742e92103a9a
iresville <iresgh1@gmail.com>
2023-02-18 21:00:46 -0800

    printalphabets

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /** 
-main - Entry point
-Description: prints char below 10
-Return: Always 0 (success)
+*main - Entry point
+*Description: prints char below 10
+*Return: Always 0 (success)
 */
 int main(void)
 {
@@ -15,4 +15,3 @@ putchar(a + '0');
 putchar('\n');
 return (0);
 }
-
