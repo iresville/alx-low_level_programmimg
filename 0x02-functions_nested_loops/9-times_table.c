ac364169d52a8b91a165c7ba29e1a6d9a8d3f3f0
iresville <iresgh1@gmail.com>
2023-02-22 09:33:37 -0800

    functions time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table
- * Description: prints the 9 times table
- * Return: void
- */
+*times_table - prints the 9 times table
+*Description: prints the 9 times table
+*Return: void
+*/
 void times_table(void)
 {
 int i, j, res;
@@ -24,4 +24,3 @@ _putchar((res % 10) + '0');
 _putchar('\n');
 }
 }
-return (0);
