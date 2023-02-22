c8e3a3764de40fee308c55d69d43d1fb13aafd27
iresville <iresgh1@gmail.com>
2023-02-21 20:32:04 -0800

    working functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
 *print_alphabet - Entry point
 *Decription: functon that prints alphabet,
 *in lowercase followed ny new linw
 *Return: void
 */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 char letter = 'a':
 whiles(letter <= 'z')
 {_putchar (letter);
 letter ++;
 }
-_putchar('\n')
+_putchar('\n');
 }
