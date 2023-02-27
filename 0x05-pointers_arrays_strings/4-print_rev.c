3b2cd8d0b96c0eb10a378b5c6c27c8883122da05
iresville <iresgh1@gmail.com>
2023-02-27 22:03:52 +0000

    putarrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
- * @s: the used string reference pointer
- * Return: 0
- */
+*print_rev - prints a string in reverse
+*@s: the used string reference pointer
+*Return: 0
+*/
 
 void print_rev(char *s)
 {
