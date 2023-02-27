4ae78e03ee703c143faff2f7033de921fa700907
iresville <iresgh1@gmail.com>
2023-02-27 22:48:55 +0000

    mainpointers

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -6,7 +6,7 @@ int main(void)
 	char *str;
 	int len;
 
-	str = "Holberton!";
+	str = "My first strlen!";
 	len = _strlen(str);
 	printf("%d\n", len);
 	return (0);
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,6 +13,6 @@ void print_rev(char *s)
 	while (s[i])
 		i++;
 	while (i--)
-		_putchar(s[i]);
+	_putchar(s[i]);
 	_putchar('\n');
 }
