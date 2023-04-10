55c60ac51e2eaf0f254ebc65eb47fcdfddd55729
iresville <iresgh1@gmail.com>
2023-04-10 08:20:05 -0700

    create_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,18 +8,18 @@
 * Return: w- actual no. of bytes read and printed
 * Return: 0 when function fails or filename is NULL.
 */
-ssize_t read_textfile(const char *filename, ssize_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
+char *buffer;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buffer = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
+buffer = malloc(sizeof(char) *letters);
+t = read(fd, buffer, letters);
+w = write(STDOUT_FILENO, buffer, t);
 free(buffer);
 close(fd);
 return (w);
